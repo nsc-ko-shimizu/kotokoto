bc65230814e5c87d5ed80719c5416f4896e7f169
nsc-ko-shimizu <63280526+nsc-ko-shimizu@users.noreply.github.com>
2020-04-09 18:23:56 +0900

    4月9日に行った課題

---
+++ area.js
@@ -0,0 +1,27 @@
+/*
+ * 四角の面積を計算してください。
+ *
+ * length: 縦の長さ
+ * width: 横の長さ
+ */
+function getArea(length, width) {
+    let area;
+
+    return area;
+
+}
+
+console.log(length*width);
+
+/**
+ * 四角の周囲を計算してください。
+ *
+ *
+ * length: 縦の長さ
+ * width: 横の長さ
+ */
+function getPerimeter(length, width) {
+    let perimeter
+
+    return perimeter
+}
